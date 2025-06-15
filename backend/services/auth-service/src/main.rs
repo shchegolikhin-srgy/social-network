2c195242c8d0238dc3a24f86347d01b1c69d5b17
Sergey <shchegolikhin.sergey@gmail.com>
2025-06-15 16:23:56 +0300

    Update main.rs

--- backend/services/auth-service/src/main.rs
+++ backend/services/auth-service/src/main.rs
@@ -1,36 +1,15 @@
-use auth_service::models::user::User;
-use axum::extract::State;
-use axum::{
-    Router,
-    serve,
-};
-use auth_service::api::routes;
-use auth_service::AppState;
-use auth_service::services::register_service;
+
+use auth_service::{AppState, run_server};
 use auth_service::core::config::Settings;
-use sqlx::Error;
+use std::sync::Arc;
 
 #[tokio::main]
-async fn main(){
-    let settings = Settings::new().await.unwrap();
+async fn main()-> Result<(), Box<dyn std::error::Error>>{
+    let settings = Settings::new().await?;
 
-    let state = AppState::new(&settings.database_url).await.unwrap();
-    
-    let app:Router = Router::new()
-        .with_state(state.clone())
-        .merge(routes::router());
-    let listener = tokio::net::TcpListener::bind(&settings.addr).await.unwrap();
-    use auth_service::services::register_service;
-    let conn:Result<(), Error> = register_service::register_user_by_username(State(state.clone()), User{
-        hashed_password: String::from("password"),
-        username: String::from("pidor@"),
-        email: String::from("example@"),
-    }).await;
-    match conn {
-        Ok(())=>println!("Ok"),
-        _ =>println!("Failed!!!!"),
-    }
-    
+    let app_state = AppState::new(&settings.database_url).await?; 
+    let state = Arc::new(app_state);
+    run_server(state, settings).await?;
 
-    serve(listener, app).await.unwrap();
+    Ok(())
 }
